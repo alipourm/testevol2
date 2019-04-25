be31017387f6d1e399d4d3b5cd5a5117e9a531d8
Amin Alipour <aminalipour@gmail.com>
2019-04-25 18:48:23 -0500

    .

--- src/main/java/Differencer.java
+++ src/main/java/Differencer.java
@@ -1,15 +1,26 @@
+import gumtree.spoon.diff.Diff;
+import gumtree.spoon.diff.operations.Operation;
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.eclipse.jgit.diff.DiffEntry;
 
+import gumtree.spoon.*;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -20,13 +31,17 @@ public class Differencer implements Task {
     List<RevCommit> revisions;
     Git git;
     Repository repository;
+    TreeWalk treeWalk;
 
     public Differencer(Git _git) throws GitAPIException {
         git = _git;
         this.getRevisions();
         repository = git.getRepository();
-
+        treeWalk = new TreeWalk(repository);
     }
+
+
+
 
 
     public void getRevisions() throws GitAPIException {
@@ -43,6 +58,44 @@ public class Differencer implements Task {
     public void setRevisions(List revisions){
         this.revisions = revisions;
     }
+
+    public void astDiff(ObjectId newObj, ObjectId oldObj, String filePath){
+        TreeWalk treeWalk = new TreeWalk(repository);
+
+        // head
+        try {
+
+
+            File oldTmpFile = File.createTempFile("old", ".java");
+            File newTmpFile = File.createTempFile("old", ".java");
+
+            ObjectLoader newLoader = repository.open(newObj);
+            newLoader.copyTo(new FileOutputStream(newTmpFile));
+
+            ObjectLoader oldLoader = repository.open(oldObj);
+            oldLoader.copyTo(new FileOutputStream(oldTmpFile));
+
+            Diff astDiffs = new AstComparator().compare(newTmpFile, oldTmpFile);
+//            Iterator<> it = astDiffs.getAllOperations().iterator();
+            System.out.println("Changes are:");
+
+            for (Operation op:astDiffs.getRootOperations()){
+                System.out.println("op "+op);
+
+             //   System.out.println(op.getSrcNode().toString());
+            //    System.out.println(op.getDstNode().toString());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
     public void diffFiles(RevCommit head, RevCommit oldHead){
         try {
@@ -63,6 +116,24 @@ public class Differencer implements Task {
                 for (DiffEntry entry : diffs) {
                     System.out.println("Entry: " + entry);
 
+                    if (entry.toString().trim().endsWith("java]"))
+                    {
+
+
+                        // This is only for MODIFY but we have ADD, MOV, RM too.
+                        DiffEntry.ChangeType changeType = entry.getChangeType();
+
+                        if (changeType ==DiffEntry.ChangeType.MODIFY
+                            //    changeType==DiffEntry.ChangeType.ADD
+                        )
+                        {
+                            String st = entry.getNewPath();
+                            ObjectId newObjectId = entry.getNewId().toObjectId();
+                            ObjectId oldObjectId = entry.getOldId().toObjectId();
+                            astDiff(newObjectId, oldObjectId, "");
+
+                        }
+                    }
                 }
             } catch (GitAPIException e) {
                 e.printStackTrace();
@@ -81,7 +152,7 @@ public class Differencer implements Task {
         for(int i = 0; i < revisions.size() - 1; i++){
             diffFiles(revisions.get(i), revisions.get(i+1));
             RevCommit base = revisions.get(i);
-            System.out.println("#######" + i + " " + new Time(revisions.get(i).getCommitTime()) + " " + base.getShortMessage());
+           System.out.println("#######" + i + " " + new Time(revisions.get(i).getCommitTime()) + " " + base.getShortMessage());
 
         }
 
